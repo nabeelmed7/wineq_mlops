ce1690fcb9daefdc8fe5b9de542aa870cca39487
Nabeel <beelkaahmed@gmail.com>
2023-01-11 22:33:52 -0500

    Updated README

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'src',
+    version = '0.0.1',
+    description = 'Wine quality package',
+    author = 'Nabeel',
+    packages = find_packages(),
+    license = 'MIT'
+)
